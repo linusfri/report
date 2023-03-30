e3dffa31c0c0cde146404be03a3a9013c2206b89
Linus Fri <linus.f@protonmail.ch>
2023-03-30 20:37:02 +0200

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', 'home')]
+    public function Home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route('/about', 'about')]
+    public function About(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+}
---
+++ src/Controller/LuckyControllerTwig.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyControllerTwig extends AbstractController
+{
+    #[Route('/lucky/number/twig', name:'lucky_number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'number' => $number
+        ];
+
+        return $this->render('lucky_number.html.twig', $data);
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+}
